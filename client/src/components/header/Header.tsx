cf36750879d9c5bd729285475e8c1113f8970309
Al omran Abduljaber <alomranabduljaber@gmail.com>
2026-03-23 19:23:13 +0100

    Add sequence diagram

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -10,34 +10,73 @@ export default function Header() {
     navigate("/login");
   };
   return (
-    <div className="navbar bg-base-100 shadow-sm px-4">
-  {/* LOGO */}
+    <div className="navbar bg-base-100 shadow-sm">
+      <div className="flex-1">
+        <Link to="/" className="btn btn-ghost text-xl">
+          TaskFlow
+        </Link>
+      </div>
 
+      <div className="flex-1 justify-center hidden md:flex">
+        <input
+          type="text"
+          placeholder="Rechercher"
+          className="input input-bordered w-full max-w-xs rounded-xl outline-none"
+        />
+      </div>
 
-  {/* RECHERCHE (Adaptative) */}
-  <div className="flex-none gap-2">
-   
+      <div className="flex-1 justify-end flex gap-2 items-center">
+        <input
+          type="text"
+          placeholder="Rechercher"
+          className="input input-bordered w-37 md:hidden rounded-xl outline-none"
+        />
+        {user ? (
+          <div className="dropdown dropdown-end">
+            <button
+              tabIndex={0}
+              className="btn btn-ghost btn-circle avatar"
+              type="button"
+            >
+              <div className="w-10 rounded-full">
+                <img
+                  alt="avatar"
+                  src={`https://ui-avatars.com/api/?name=${user.username}&background=random`}
+                />
+              </div>
+            </button>
 
-    {/* UTILISATEUR OU BOUTONS */}
-    {user ? (
-      <div className="dropdown dropdown-end">
-        <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-          <div className="w-10 rounded-full">
-            <img src={`https://ui-avatars.com/api/?name=${user.username}&background=random`} alt="user" />
+            <ul
+              tabIndex={-1}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            >
+              <li>
+                <Link to="/profile" className="justify-between">
+                  Profil
+                </Link>
+              </li>
+              <li>
+                <button
+                  type="button"
+                  onClick={handleLogout}
+                  className="text-red-500 text-left"
+                >
+                  Déconnexion
+                </button>
+              </li>
+            </ul>
           </div>
-        </label>
-        <ul tabIndex={0} className="mt-3  p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
-          <li><Link to="/profile">Profil</Link></li>
-          <li><button onClick={handleLogout} className="text-red-500">Déconnexion</button></li>
-        </ul>
+        ) : (
+          <div className="flex gap-2">
+            <Link to="/login" className="btn btn-ghost">
+              Connexion
+            </Link>
+            <Link to="/register" className="btn btn-primary">
+              Inscription
+            </Link>
+          </div>
+        )}
       </div>
-    ) : (
-      <div className="flex gap-1">
-        <Link to="/login" className="btn btn-ghost btn-sm">Connexion</Link>
-        <Link to="/register" className="btn btn-primary btn-sm">S'inscrire</Link>
-      </div>
-    )}
-  </div>
-</div>
+    </div>
   );
 }
